bcfc6f77f5afab9084e0c7e4cda6c1e06cc42f65
Chris Malloy <cjmalloy@gmail.com>
2022-03-18 14:44:29 -0300

    Init

---
+++ src/test/java/ca/hc/jasper/JasperApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.hc.jasper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
