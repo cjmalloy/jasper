4242019df8dc45c4783a27a58e6da4c070d8ff64
Chris Malloy <cjmalloy@gmail.com>
2022-03-27 16:02:25 -0300

    Added RefService tests

--- src/test/java/ca/hc/jasper/service/RefServiceIT.java
+++ src/test/java/ca/hc/jasper/service/RefServiceIT.java
@@ -33,55 +33,57 @@ public class RefServiceIT {
 	@Autowired
 	UserRepository userRepository;
 
+	static final String URL = "https://www.example.com/";
+
 	@Test
 	void testCreateUntaggedRef() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setPublished(Instant.now());
 
 		refService.create(ref);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
 	}
 
 	@Test
 	void testCreateRefWithPublicTag() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTags(List.of("public"));
 		ref.setPublished(Instant.now());
 
 		refService.create(ref);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
 	}
 
 	@Test
 	void testCreateRefWithReadableTags() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTags(List.of("public", "custom", "tags"));
 		ref.setPublished(Instant.now());
 
 		refService.create(ref);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
 	}
 
 	@Test
 	void testCreateRefWithUnreadableTagsFails() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTags(List.of("_secret"));
 		ref.setPublished(Instant.now());
 
 		assertThatThrownBy(() -> refService.create(ref))
 			.isInstanceOf(AccessDeniedException.class);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isFalse();
 	}
 
@@ -92,23 +94,68 @@ public class RefServiceIT {
 		user.setReadAccess(List.of("_secret"));
 		userRepository.save(user);
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTags(List.of("_secret"));
 		ref.setPublished(Instant.now());
 
 		refService.create(ref);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		assertThat(refRepository.findOneByUrlAndOrigin("https://www.example.com/", "")
-			.get().getTags())
+		assertThat(refRepository.findOneByUrlAndOrigin(URL, "").get().getTags())
 			.containsExactly("_secret");
+	}
+
+	@Test
+	void testCreateRefWithUserTags() {
+		var ref = new Ref();
+		ref.setUrl(URL);
+		ref.setTags(List.of("user/tester"));
+		ref.setPublished(Instant.now());
+
+		refService.create(ref);
+
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
+			.isTrue();
+		assertThat(refRepository.findOneByUrlAndOrigin(URL, "").get().getTags())
+			.containsExactly("user/tester");
+	}
+
+	@Test
+	@WithMockUser(value = "tester", roles = {"USER", "PRIVATE"})
+	void testCreateRefWithPrivateUserTags() {
+		var ref = new Ref();
+		ref.setUrl(URL);
+		ref.setTags(List.of("_user/tester"));
+		ref.setPublished(Instant.now());
+
+		refService.create(ref);
+
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
+			.isTrue();
+		assertThat(refRepository.findOneByUrlAndOrigin(URL, "").get().getTags())
+			.containsExactly("_user/tester");
+	}
+
+	@Test
+	@WithMockUser(value = "tester", roles = {"USER", "PRIVATE"})
+	void testCreateRefWithPrivateUserTagsFailed() {
+		var ref = new Ref();
+		ref.setUrl(URL);
+		ref.setTags(List.of("_user/other"));
+		ref.setPublished(Instant.now());
+
+		assertThatThrownBy(() -> refService.create(ref))
+			.isInstanceOf(AccessDeniedException.class);
+
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
+			.isFalse();
 	}
 
 	@Test
 	void testGetPageUntaggedRef() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
 
@@ -123,7 +170,7 @@ public class RefServiceIT {
 	@Test
 	void testGetPageRefWithPublicTag() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTags(List.of("public"));
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
@@ -143,7 +190,7 @@ public class RefServiceIT {
 		user.setReadAccess(List.of("custom"));
 		userRepository.save(user);
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTags(List.of("custom"));
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
@@ -163,7 +210,7 @@ public class RefServiceIT {
 		user.setReadAccess(List.of("_secret"));
 		userRepository.save(user);
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTags(List.of("_secret"));
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
@@ -181,7 +228,7 @@ public class RefServiceIT {
 	@Test
 	void testGetPageRefWithUnreadablePrivateTags() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTags(List.of("_secret"));
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
@@ -197,7 +244,7 @@ public class RefServiceIT {
 	@Test
 	void testGetPageRefFiltersUnreadablePrivateTags() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTags(List.of("public", "_secret"));
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
@@ -215,21 +262,21 @@ public class RefServiceIT {
 	@Test
 	void testUpdateUntaggedRef() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
 		var update = new Ref();
-		update.setUrl("https://www.example.com/");
+		update.setUrl(URL);
 		update.setTitle("Second");
 		update.setPublished(Instant.now());
 
 		assertThatThrownBy(() -> refService.update(update))
 			.isInstanceOf(AccessDeniedException.class);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("First");
 	}
@@ -237,13 +284,13 @@ public class RefServiceIT {
 	@Test
 	void testUpdateRefWithPublicTag() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("public"));
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
 		var update = new Ref();
-		update.setUrl("https://www.example.com/");
+		update.setUrl(URL);
 		update.setTitle("Second");
 		update.setTags(List.of("public"));
 		update.setPublished(Instant.now());
@@ -251,9 +298,9 @@ public class RefServiceIT {
 		assertThatThrownBy(() -> refService.update(update))
 			.isInstanceOf(AccessDeniedException.class);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("First");
 	}
@@ -261,22 +308,22 @@ public class RefServiceIT {
 	@Test
 	void testUpdateRefWithUserTag() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("user/tester"));
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
 		var update = new Ref();
-		update.setUrl("https://www.example.com/");
+		update.setUrl(URL);
 		update.setTitle("Second");
 		ref.setTags(List.of("user/tester"));
 		update.setPublished(Instant.now());
 
 		refService.update(update);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("Second");
 	}
@@ -288,13 +335,13 @@ public class RefServiceIT {
 		user.setReadAccess(List.of("custom"));
 		userRepository.save(user);
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("custom"));
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
 		var update = new Ref();
-		update.setUrl("https://www.example.com/");
+		update.setUrl(URL);
 		update.setTitle("Second");
 		update.setTags(List.of("custom"));
 		update.setPublished(Instant.now());
@@ -302,9 +349,9 @@ public class RefServiceIT {
 		assertThatThrownBy(() -> refService.update(update))
 			.isInstanceOf(AccessDeniedException.class);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("First");
 	}
@@ -316,22 +363,22 @@ public class RefServiceIT {
 		user.setWriteAccess(List.of("custom"));
 		userRepository.save(user);
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("custom"));
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
 		var update = new Ref();
-		update.setUrl("https://www.example.com/");
+		update.setUrl(URL);
 		update.setTitle("Second");
 		update.setTags(List.of("custom"));
 		update.setPublished(Instant.now());
 
 		refService.update(update);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("Second");
 	}
@@ -339,22 +386,22 @@ public class RefServiceIT {
 	@Test
 	void testUpdateRefWithUnreadablePrivateTags() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("_secret"));
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
 		var update = new Ref();
-		update.setUrl("https://www.example.com/");
+		update.setUrl(URL);
 		update.setTitle("Second");
 		update.setPublished(Instant.now());
 
 		assertThatThrownBy(() -> refService.update(update))
 			.isInstanceOf(AccessDeniedException.class);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("First");
 	}
@@ -366,22 +413,22 @@ public class RefServiceIT {
 		user.setReadAccess(List.of("_secret"));
 		userRepository.save(user);
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("_secret"));
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
 		var update = new Ref();
-		update.setUrl("https://www.example.com/");
+		update.setUrl(URL);
 		update.setTitle("Second");
 		update.setPublished(Instant.now());
 
 		assertThatThrownBy(() -> refService.update(update))
 			.isInstanceOf(AccessDeniedException.class);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("First");
 	}
@@ -393,21 +440,21 @@ public class RefServiceIT {
 		user.setWriteAccess(List.of("_secret"));
 		userRepository.save(user);
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("_secret"));
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
 		var update = new Ref();
-		update.setUrl("https://www.example.com/");
+		update.setUrl(URL);
 		update.setTitle("Second");
 		update.setPublished(Instant.now());
 
 		refService.update(update);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("Second");
 	}
@@ -415,7 +462,7 @@ public class RefServiceIT {
 	@Test
 	void testDeleteUntaggedRef() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setPublished(Instant.now());
 		refRepository.save(ref);
@@ -423,9 +470,9 @@ public class RefServiceIT {
 		assertThatThrownBy(() -> refService.delete(ref.getUrl()))
 			.isInstanceOf(AccessDeniedException.class);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("First");
 	}
@@ -433,7 +480,7 @@ public class RefServiceIT {
 	@Test
 	void testDeleteRefWithPublicTag() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("public"));
 		ref.setPublished(Instant.now());
@@ -442,9 +489,9 @@ public class RefServiceIT {
 		assertThatThrownBy(() -> refService.delete(ref.getUrl()))
 			.isInstanceOf(AccessDeniedException.class);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("First");
 	}
@@ -452,7 +499,7 @@ public class RefServiceIT {
 	@Test
 	void testDeleteRefWithUserTag() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("user/tester"));
 		ref.setPublished(Instant.now());
@@ -460,7 +507,7 @@ public class RefServiceIT {
 
 		refService.delete(ref.getUrl());
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isFalse();
 	}
 
@@ -471,7 +518,7 @@ public class RefServiceIT {
 		user.setReadAccess(List.of("custom"));
 		userRepository.save(user);
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("custom"));
 		ref.setPublished(Instant.now());
@@ -480,9 +527,9 @@ public class RefServiceIT {
 		assertThatThrownBy(() -> refService.delete(ref.getUrl()))
 			.isInstanceOf(AccessDeniedException.class);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("First");
 	}
@@ -494,7 +541,7 @@ public class RefServiceIT {
 		user.setWriteAccess(List.of("custom"));
 		userRepository.save(user);
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("custom"));
 		ref.setPublished(Instant.now());
@@ -502,14 +549,14 @@ public class RefServiceIT {
 
 		refService.delete(ref.getUrl());
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isFalse();
 	}
 
 	@Test
 	void testDeleteRefWithUnreadablePrivateTags() {
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("_secret"));
 		ref.setPublished(Instant.now());
@@ -518,9 +565,9 @@ public class RefServiceIT {
 		assertThatThrownBy(() -> refService.delete(ref.getUrl()))
 			.isInstanceOf(AccessDeniedException.class);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("First");
 	}
@@ -532,7 +579,7 @@ public class RefServiceIT {
 		user.setReadAccess(List.of("_secret"));
 		userRepository.save(user);
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("_secret"));
 		ref.setPublished(Instant.now());
@@ -541,9 +588,9 @@ public class RefServiceIT {
 		assertThatThrownBy(() -> refService.delete(ref.getUrl()))
 			.isInstanceOf(AccessDeniedException.class);
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isTrue();
-		var fetched = refRepository.findOneByUrlAndOrigin("https://www.example.com/", "").get();
+		var fetched = refRepository.findOneByUrlAndOrigin(URL, "").get();
 		assertThat(fetched.getTitle())
 			.isEqualTo("First");
 	}
@@ -555,7 +602,7 @@ public class RefServiceIT {
 		user.setWriteAccess(List.of("_secret"));
 		userRepository.save(user);
 		var ref = new Ref();
-		ref.setUrl("https://www.example.com/");
+		ref.setUrl(URL);
 		ref.setTitle("First");
 		ref.setTags(List.of("_secret"));
 		ref.setPublished(Instant.now());
@@ -563,7 +610,7 @@ public class RefServiceIT {
 
 		refService.delete(ref.getUrl());
 
-		assertThat(refRepository.existsByUrlAndOrigin("https://www.example.com/", ""))
+		assertThat(refRepository.existsByUrlAndOrigin(URL, ""))
 			.isFalse();
 	}
 }
