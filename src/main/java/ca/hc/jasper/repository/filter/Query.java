450571e004c67856864cbf9d7f2a7c62f783536d
Chris Malloy <cjmalloy@gmail.com>
2022-04-11 02:21:52 -0300

    Removed + as OR query delim

--- src/main/java/ca/hc/jasper/repository/filter/Query.java
+++ src/main/java/ca/hc/jasper/repository/filter/Query.java
@@ -4,9 +4,9 @@ import ca.hc.jasper.repository.spec.QualifiedTag;
 
 public interface Query {
 	String AND_REGEX = "[:&]";
-	String OR_REGEX = "[ +|]";
-	String DELIMS = "[ +|:&]";
+	String OR_REGEX = "[ |]";
+	String DELIMS = "[ |:&]";
 	String SELECTOR = "!?" + QualifiedTag.REGEX;
-	String REGEX = SELECTOR + "([ +|:&]" + SELECTOR + ")*";
+	String REGEX = SELECTOR + "([ |:&]" + SELECTOR + ")*";
 	String getQuery();
 }
