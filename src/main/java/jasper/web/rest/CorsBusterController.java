0f510a194f4779c514f53aed1ed7119a1b7494c7
Chris Malloy <cjmalloy@gmail.com>
2022-06-09 13:58:17 -0300

    Only submitters need cors proxy

--- src/main/java/jasper/web/rest/CorsBusterController.java
+++ src/main/java/jasper/web/rest/CorsBusterController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import jasper.client.BitChuteClient;
 import jasper.client.TwitterClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("api/v1/cors")
 @Validated
+@PreAuthorize("hasRole('USER')")
 public class CorsBusterController {
 
 	@Autowired
