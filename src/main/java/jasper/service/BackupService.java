cb2e134383158673e5fb0fa0d0e05e03d4b1a5e6
Chris Malloy <cjmalloy@gmail.com>
2022-08-14 09:06:32 -0300

    Allow mods to download backups

--- src/main/java/jasper/service/BackupService.java
+++ src/main/java/jasper/service/BackupService.java
@@ -14,35 +14,40 @@ import java.util.List;
 
 @Profile("storage")
 @Service
-@PreAuthorize("hasRole('ADMIN')")
 public class BackupService {
 
 	@Autowired
 	Backup backup;
 
+	@PreAuthorize("hasRole('ADMIN')")
 	public String createBackup(BackupOptionsDto options) throws IOException {
 		var id = Instant.now().toString();
 		backup.createBackup(id, options);
 		return id;
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	public void uploadBackup(String id, byte[] zipFile) throws IOException {
 		backup.store(id, zipFile);
 	}
 
+	@PreAuthorize("hasRole('MOD')")
 	public List<String> listBackups() {
 		return backup.listBackups();
 	}
 
+	@PreAuthorize("hasRole('MOD')")
 	public byte[] getBackup(String id) {
 		return backup.get(id);
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	public void restoreBackup(String id, BackupOptionsDto options) {
 		if (!backup.exists(id)) throw new NotFoundException("Backup " + id);
 		backup.restore(id, options);
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	public void deleteBackup(String id) throws IOException {
 		if (!backup.exists(id)) return; // Delete is idempotent
 		backup.delete(id);
