b73936e5b3570acea88ebad35ae9f820fca654c9
Chris Malloy <cjmalloy@gmail.com>
2023-04-02 19:14:52 -0300

    Repl cursor origin should be in query

--- src/main/java/jasper/client/JasperClient.java
+++ src/main/java/jasper/client/JasperClient.java
@@ -20,35 +20,35 @@ public interface JasperClient {
 
 	@RequestLine("GET /api/v1/repl/ref")
 	List<Ref> refPull(URI baseUri, @QueryMap Map<String, Object> params);
-	@RequestLine("GET /api/v1/repl/ref/cursor/{origin}")
+	@RequestLine("GET /api/v1/repl/ref/cursor?origin={origin}")
 	Instant refCursor(URI baseUri, @Param("origin") String origin);
 	@RequestLine("POST /api/v1/repl/ref")
 	void refPush(URI baseUri, List<Ref> push);
 
 	@RequestLine("GET /api/v1/repl/ext")
 	List<Ext> extPull(URI baseUri, @QueryMap Map<String, Object> params);
-	@RequestLine("GET /api/v1/repl/ext/cursor/{origin}")
+	@RequestLine("GET /api/v1/repl/ext/cursor?origin={origin}")
 	Instant extCursor(URI baseUri, @Param("origin") String origin);
 	@RequestLine("GET /api/v1/repl/ext")
 	void extPush(URI baseUri, List<Ext> push);
 
 	@RequestLine("GET /api/v1/repl/user")
 	List<User> userPull(URI baseUri, @QueryMap Map<String, Object> params);
-	@RequestLine("GET /api/v1/repl/user/cursor/{origin}")
+	@RequestLine("GET /api/v1/repl/user/cursor?origin=origin}")
 	Instant userCursor(URI baseUri, @Param("origin") String origin);
 	@RequestLine("GET /api/v1/repl/user")
 	void userPush(URI baseUri, List<User> push);
 
 	@RequestLine("GET /api/v1/repl/plugin")
 	List<Plugin> pluginPull(URI baseUri, @QueryMap Map<String, Object> params);
-	@RequestLine("GET /api/v1/repl/plugin/cursor/{origin}")
+	@RequestLine("GET /api/v1/repl/plugin/cursor?origin={origin}")
 	Instant pluginCursor(URI baseUri, @Param("origin") String origin);
 	@RequestLine("GET /api/v1/repl/plugin")
 	void pluginPush(URI baseUri, List<Plugin> push);
 
 	@RequestLine("GET /api/v1/repl/template")
 	List<Template> templatePull(URI baseUri, @QueryMap Map<String, Object> params);
-	@RequestLine("GET /api/v1/repl/template/cursor/{origin}")
+	@RequestLine("GET /api/v1/repl/template/cursor?origin={origin}")
 	Instant templateCursor(URI baseUri, @Param("origin") String origin);
 	@RequestLine("GET /api/v1/repl/template")
 	void templatePush(URI baseUri, List<Template> push);
