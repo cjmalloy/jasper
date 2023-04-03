51c4818c7ad68ee3607b11e6091464a9910f6082
Chris Malloy <cjmalloy@gmail.com>
2023-04-02 21:06:31 -0300

    Whitespace

--- src/main/java/jasper/component/TunnelClient.java
+++ src/main/java/jasper/component/TunnelClient.java
@@ -25,57 +25,56 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 @Component
 public class TunnelClient {
-    private static final Logger logger = LoggerFactory.getLogger(TunnelClient.class);
+	private static final Logger logger = LoggerFactory.getLogger(TunnelClient.class);
 
-    @Autowired
-    UserRepository userRepository;
+	@Autowired
+	UserRepository userRepository;
 
-    @Autowired
-    ObjectMapper objectMapper;
+	@Autowired
+	ObjectMapper objectMapper;
 
-    public void proxy(Ref remote, ProxyRequest request) {
-        var config = objectMapper.convertValue(remote.getPlugins().get("+plugin/origin"), Origin.class);
-        URI url = null;
-        try {
-            url = new URI(isNotBlank(config.getProxy()) ? config.getProxy() : remote.getUrl());
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-        var tunnel = objectMapper.convertValue(remote.getPlugins().get("+plugin/origin/tunnel"), Tunnel.class);
-        if (tunnel == null) {
-            request.go(url);
-        } else {
-            var user = userRepository.findOneByQualifiedTag(tunnel.getUser());
-            if (user.isEmpty() || user.get().getKey() == null) {
-                logger.error("Tunnel requested, but user {} does not have a private key set.", tunnel.getUser());
-                return;
-            }
-            try (var client = SshClient.setUpDefaultClient()) {
-                client.start();
-                var host = isNotBlank(tunnel.getSshHost()) ? tunnel.getSshHost() : url.getHost();
-                try (var session = client.connect("root", host, 22)
-                        .verify(30, TimeUnit.SECONDS).getSession()) {
-                    var pks = SecurityUtils.loadKeyPairIdentities(null, NamedResource.ofName("root"), new ByteArrayInputStream(user.get().getKey()), null);
-                    pks.forEach(session::addPublicKeyIdentity);
-                    session.auth().verify(30, TimeUnit.SECONDS);
-                    try (var tracker = session.createLocalPortForwardingTracker(38080, new SshdSocketAddress("localhost", 8022))) {
+	public void proxy(Ref remote, ProxyRequest request) {
+		var config = objectMapper.convertValue(remote.getPlugins().get("+plugin/origin"), Origin.class);
+		URI url = null;
+		try {
+			url = new URI(isNotBlank(config.getProxy()) ? config.getProxy() : remote.getUrl());
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+		var tunnel = objectMapper.convertValue(remote.getPlugins().get("+plugin/origin/tunnel"), Tunnel.class);
+		if (tunnel == null) {
+			request.go(url);
+		} else {
+			var user = userRepository.findOneByQualifiedTag(tunnel.getUser());
+			if (user.isEmpty() || user.get().getKey() == null) {
+				logger.error("Tunnel requested, but user {} does not have a private key set.", tunnel.getUser());
+				return;
+			}
+			try (var client = SshClient.setUpDefaultClient()) {
+				client.start();
+				var host = isNotBlank(tunnel.getSshHost()) ? tunnel.getSshHost() : url.getHost();
+				try (var session = client.connect("root", host, 22).verify(30, TimeUnit.SECONDS).getSession()) {
+					var pks = SecurityUtils.loadKeyPairIdentities(null, NamedResource.ofName("root"), new ByteArrayInputStream(user.get().getKey()), null);
+					pks.forEach(session::addPublicKeyIdentity);
+					session.auth().verify(30, TimeUnit.SECONDS);
+					try (var tracker = session.createLocalPortForwardingTracker(38080, new SshdSocketAddress("localhost", 8022))) {
 						logger.debug("Opened reverse proxy in SSH tunnel.");
-                        request.go(new URI("http://localhost:38080"));
-                    } catch (URISyntaxException e) {
-                        throw new RuntimeException(e);
-                    }
-                } catch (IOException | GeneralSecurityException e) {
-                    throw new RuntimeException(e);
-                } finally {
-                    client.stop();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
+						request.go(new URI("http://localhost:38080"));
+					} catch (URISyntaxException e) {
+						throw new RuntimeException(e);
+					}
+				} catch (IOException | GeneralSecurityException e) {
+					throw new RuntimeException(e);
+				} finally {
+					client.stop();
+				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
 
-    public interface ProxyRequest {
-        void go(URI url);
-    }
+	public interface ProxyRequest {
+		void go(URI url);
+	}
 }
