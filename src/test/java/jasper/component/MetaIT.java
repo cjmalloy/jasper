9bc917a2ad8fb8de046a6a80e3ee37d415f7328a
Chris Malloy <cjmalloy@gmail.com>
2023-04-05 05:22:41 -0300

    Added metadata tests

---
+++ src/test/java/jasper/component/MetaIT.java
@@ -0,0 +1,115 @@
+package jasper.component;
+
+import jasper.IntegrationTest;
+import jasper.domain.Plugin;
+import jasper.domain.Ref;
+import jasper.repository.PluginRepository;
+import jasper.repository.RefRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@IntegrationTest
+@Transactional
+public class MetaIT {
+
+	@Autowired
+	Meta meta;
+
+	@Autowired
+	RefRepository refRepository;
+
+	@Autowired
+	PluginRepository pluginRepository;
+
+	static final String URL = "https://www.example.com/";
+
+	@Test
+	void testCreateMetadata() {
+		var ref = new Ref();
+		ref.setUrl(URL);
+		ref.setTitle("First");
+		ref.setTags(List.of("+user/tester"));
+
+		meta.update(ref, null, null);
+
+		assertThat(ref.getMetadata().getResponses()).isEmpty();
+		assertThat(ref.getMetadata().getInternalResponses()).isEmpty();
+		assertThat(ref.getMetadata().getPlugins()).isEmpty();
+	}
+
+	@Test
+	void testCreateMetadataResponse() {
+		var ref = new Ref();
+		ref.setUrl(URL);
+		ref.setTitle("First");
+		ref.setTags(List.of("+user/tester"));
+		refRepository.save(ref);
+		var child = new Ref();
+		child.setUrl(URL + 2);
+		child.setTitle("Child");
+		child.setSources(List.of(URL));
+		child.setTags(List.of("+user/tester"));
+
+		meta.update(child, null, null);
+
+		var parent = refRepository.findOneByUrlAndOrigin(URL, "");
+		assertThat(parent).isNotEmpty();
+		assertThat(parent.get().getMetadata().getResponses()).containsExactly(URL+2);
+		assertThat(parent.get().getMetadata().getInternalResponses()).isEmpty();
+		assertThat(parent.get().getMetadata().getPlugins()).isEmpty();
+	}
+
+	@Test
+	void testCreateMetadataInternalResponse() {
+		var ref = new Ref();
+		ref.setUrl(URL);
+		ref.setTitle("First");
+		ref.setTags(List.of("+user/tester"));
+		refRepository.save(ref);
+		var child = new Ref();
+		child.setUrl(URL + 2);
+		child.setTitle("Child");
+		child.setSources(List.of(URL));
+		child.setTags(List.of("+user/tester", "internal"));
+
+		meta.update(child, null, null);
+
+		var parent = refRepository.findOneByUrlAndOrigin(URL, "");
+		assertThat(parent).isNotEmpty();
+		assertThat(parent.get().getMetadata().getResponses()).isEmpty();
+		assertThat(parent.get().getMetadata().getInternalResponses()).containsExactly(URL+2);
+		assertThat(parent.get().getMetadata().getPlugins()).isEmpty();
+	}
+
+	@Test
+	void testCreateMetadataPluginResponse() {
+		var ref = new Ref();
+		ref.setUrl(URL);
+		ref.setTitle("First");
+		ref.setTags(List.of("+user/tester"));
+		refRepository.save(ref);
+		var comment = new Plugin();
+		comment.setTag("plugin/comment");
+		comment.setGenerateMetadata(true);
+		pluginRepository.save(comment);
+		var child = new Ref();
+		child.setUrl(URL + 2);
+		child.setTitle("Child");
+		child.setSources(List.of(URL));
+		child.setTags(List.of("+user/tester", "plugin/comment", "internal"));
+
+		meta.update(child, null, null);
+
+		var parent = refRepository.findOneByUrlAndOrigin(URL, "");
+		assertThat(parent).isNotEmpty();
+		assertThat(parent.get().getMetadata().getResponses()).isEmpty();
+		assertThat(parent.get().getMetadata().getInternalResponses()).containsExactly(URL+2);
+		assertThat(parent.get().getMetadata().getPlugins().get("plugin/comment")).containsExactly(URL+2);
+	}
+
+}
