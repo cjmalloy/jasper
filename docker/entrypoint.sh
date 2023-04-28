4be3bba17b9828d1a2e10bc7aab094c646968b2e
Chris Malloy <cjmalloy@gmail.com>
2023-04-28 08:33:01 -0300

    Use urandom in docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,1 +1,1 @@
-java  "-Xmx${JASPER_HEAP:-512m}" "-Xms${JASPER_HEAP:-512m}" org.springframework.boot.loader.JarLauncher
+java -Djava.security.egd=file:/dev/./urandom "-Xmx${JASPER_HEAP:-512m}" "-Xms${JASPER_HEAP:-512m}" org.springframework.boot.loader.JarLauncher
