1af5142f3a112f246de6cedf0e622a2ae0d4da32
Chris Malloy <cjmalloy@gmail.com>
2023-05-21 11:00:10 -0300

    Made oembed defaults an admin endpoint

--- src/main/java/jasper/component/OembedProviders.java
+++ src/main/java/jasper/component/OembedProviders.java
@@ -6,26 +6,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jasper.config.Props;
 import jasper.domain.Ref;
 import jasper.plugin.Oembed;
-import jasper.repository.RefRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-@Profile("oembed")
 @Component
 public class OembedProviders {
+	private static final Logger logger = LoggerFactory.getLogger(OembedProviders.class);
 
 	@Autowired
 	Props props;
 
 	@Autowired
-	RefRepository refRepository;
+	Ingest ingest;
 
 	@Value("classpath:providers.json")
 	Resource defaultProviders;
@@ -33,24 +33,23 @@ public class OembedProviders {
 	@Autowired
 	ObjectMapper objectMapper;
 
-	@PostConstruct
-	public void init() throws IOException {
-		create(objectMapper.readValue(defaultProviders.getFile(), new TypeReference<List<Oembed>>() {}));
+	public void defaults(String origin) throws IOException {
+		create(origin, objectMapper.readValue(defaultProviders.getFile(), new TypeReference<List<Oembed>>() {}));
 	}
 
-	public void create(List<Oembed> providers) {
-			for (var p : providers) {
-				var ref = new Ref();
-				ref.setUrl(p.getProvider_url());
-				ref.setTitle(p.getProvider_name());
-				ref.setTags(List.of("public", "internal", "+plugin/oembed"));
-				var plugins = objectMapper.createObjectNode();
-				plugins.set("+plugin/oembed", objectMapper.convertValue(p, JsonNode.class));
-				ref.setPlugins(plugins);
-				for (var origin : props.getOembedOrigins()) {
-					ref.setOrigin(origin);
-					refRepository.save(ref);
-			}
+	public void create(String origin, List<Oembed> providers) {
+		logger.info("Restoring default oEmbed providers...");
+		for (var p : providers) {
+			var ref = new Ref();
+			ref.setUrl(p.getProvider_url());
+			ref.setTitle(p.getProvider_name());
+			ref.setTags(new ArrayList<>(List.of("public", "internal", "+plugin/oembed")));
+			var plugins = objectMapper.createObjectNode();
+			plugins.set("+plugin/oembed", objectMapper.convertValue(p, JsonNode.class));
+			ref.setPlugins(plugins);
+			ref.setOrigin(origin);
+			ingest.ingest(ref, false);
 		}
+		logger.info("Done restoring default oEmbed providers.");
 	}
 }
