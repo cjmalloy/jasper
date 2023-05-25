760f768677434921602b761967b8f3c63eb12565
Chris Malloy <cjmalloy@gmail.com>
2023-05-25 14:12:55 -0300

    Scrape website contents

--- src/main/java/jasper/component/WebScraper.java
+++ src/main/java/jasper/component/WebScraper.java
@@ -26,6 +26,80 @@ public class WebScraper {
 	@Autowired
 	WebScraperClient webScraperClient;
 
+	private final String[] websiteTextSelectors = {
+		".body__inner-container",
+		".showblog-body__content",
+		".article__body-text",
+		".article__content-container",
+		".c-article-content",
+		".meteredContent",
+		".gnt_ar_b",
+		".l-article__text",
+		".article__text",
+		".sdc-article-body--story",
+		".rich-text",
+		"#main",
+		"#mainbar",
+		"#maincontent",
+		"#bodyContent",
+		"#content",
+		".wysiwyg--all-content",
+		"div[class^=article-body__content]",
+		"div[class^=article-body__container]",
+		"div[class^=c-article-body]",
+		"div[class^=ArticlePageChunksContent]",
+		"div[class^=DraftjsBlocks_draftjs]",
+		".story",
+		".blog-content",
+		".article-content",
+		".article-body",
+		".page-content",
+		".grid-body",
+		".body",
+		"article",
+		"main",
+		"section"
+	};
+
+	private final String[] removeSelectors = {
+		"noscript",
+		".ad-container",
+		".ad-unit",
+		".ad",
+		".hide-for-print",
+		".social-tools",
+		".pop-up-bar",
+		".linkstack",
+		".article-sharing",
+		".related",
+		".noprint",
+		".printfooter",
+		".mw-editsection",
+		".catlinks",
+		".video-top-container",
+		".newsletter-component",
+		".thehill-promo-link",
+		".page-actions",
+		".enhancement",
+		".article-related-inline",
+		".author-endnote-container",
+		".related_topics",
+		".share-label-text",
+		".share-toolbar-container",
+		".article-trust-bar",
+		".l-inlineStories",
+		".c-conversation-title",
+		".c-pullquote",
+		".c-signin",
+		".c-disclaimer",
+		".overlay",
+		".control",
+		".inline-video",
+		"a[href^=https://www.foxnews.com/apps-products]",
+		"div[class^=article-body__top-toolbar-container]",
+		"div[class^=article-toolbar]"
+	};
+
 	@Timed(value = "jasper.webscrape")
 	public Ref web(String url) throws IOException, URISyntaxException {
 		var result = new Ref();
@@ -36,12 +110,22 @@ public class WebScraper {
 			if (!strData.trim().startsWith("<")) return result;
 			var doc = Jsoup.parse(strData);
 			result.setTitle(doc.title());
+			for (var s : removeSelectors) {
+				doc.select(s).remove();
+			}
+			for (var s : websiteTextSelectors) {
+				if (!doc.body().select(s).isEmpty()) {
+					result.setComment(doc.body().select(s).first().html().trim());
+					return result;
+				}
+			}
+			result.setComment(doc.body().wholeText().trim());
 		}
 		return result;
 	}
 
 	@Timed(value = "jasper.webscrape")
-	public String rss(String url) throws IOException, URISyntaxException {
+	public String rss(String url) {
 		var web = fetch(url);
 		if (web.getData() == null) return null;
 		var strData = new String(web.getData());
