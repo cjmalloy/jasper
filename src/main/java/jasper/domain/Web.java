1ce515ec19f3e7cc3915321da49b5e5df3bcffc9
Chris Malloy <cjmalloy@gmail.com>
2023-07-15 14:04:52 -0300

    Add scraped date

--- src/main/java/jasper/domain/Web.java
+++ src/main/java/jasper/domain/Web.java
@@ -3,12 +3,14 @@ package jasper.domain;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.time.Instant;
 import java.util.UUID;
 
 
@@ -27,6 +29,10 @@ public class Web {
 	private String mime;
 
 	private byte[] data;
+
+	@CreatedDate
+	@Column(updatable = false)
+	private Instant scraped = Instant.now();
 
 	public static Web from(byte[] data, String mime) {
 		var result = new Web();
