578970a2dfd539858b92a7ea4b1b8e5808fc27b2
Chris Malloy <cjmalloy@gmail.com>
2023-07-15 14:05:25 -0300

    WebScraper: Allow figure tag

--- src/main/java/jasper/component/Sanitizer.java
+++ src/main/java/jasper/component/Sanitizer.java
@@ -191,6 +191,7 @@ public class Sanitizer {
 	// TODO: Allow custom protocols
 	private final Safelist whitelist = Safelist.relaxed()
 		.addTags(SVG_TAG_LIST)
+		.addTags("figure", "figcaption")
 		.addAttributes("svg", SVG_ATTRS)
 		.addAttributes("g", SVG_ATTRS)
 		.addAttributes("path", SVG_ATTRS)
