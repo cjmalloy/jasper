0816d3ea116aeea8f615061b60e1b7ec39ce292f
Chris Malloy <cjmalloy@gmail.com>
2023-07-18 16:37:31 -0300

    Periodically clear faled cache items

--- src/main/java/jasper/repository/WebRepository.java
+++ src/main/java/jasper/repository/WebRepository.java
@@ -3,7 +3,10 @@ package jasper.repository;
 import jasper.domain.Web;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface WebRepository extends JpaRepository<Web, String> {
+	void deleteAllByDataIsNullAndMimeIsNull();
 }
