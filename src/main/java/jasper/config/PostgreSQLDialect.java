16ae9654076cf94f0a5131523b9b0716e97162f9
Chris Malloy <cjmalloy@gmail.com>
2023-12-22 03:20:55 -0500

    Added postgres functions

--- src/main/java/jasper/config/PostgreSQLDialect.java
+++ src/main/java/jasper/config/PostgreSQLDialect.java
@@ -1,12 +1,18 @@
 package jasper.config;
 
+import org.hibernate.dialect.function.SQLFunctionTemplate;
 import org.hibernate.dialect.function.StandardSQLFunction;
+import org.hibernate.type.BooleanType;
 import org.hibernate.type.DurationType;
+import org.hibernate.type.StandardBasicTypes;
 import tech.jhipster.domain.util.FixedPostgreSQL95Dialect;
 
 public class PostgreSQLDialect extends FixedPostgreSQL95Dialect {
 	public PostgreSQLDialect() {
 		super();
 		registerFunction("age", new StandardSQLFunction("age", DurationType.INSTANCE));
+		registerFunction("jsonb_exists", new SQLFunctionTemplate(StandardBasicTypes.BOOLEAN, "jsonb_exists(?1, ?2)"));
+		registerFunction("jsonb_set", new SQLFunctionTemplate(StandardBasicTypes.STRING, "jsonb_set(?1, ?2, ?3, ?4)"));
+		registerFunction("cast_to_jsonb", new SQLFunctionTemplate(StandardBasicTypes.STRING, "?1::jsonb"));
 	}
 }
