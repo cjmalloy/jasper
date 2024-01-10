64f880410df5468a6f6e4ce9006c747625142ef1
Chris Malloy <cjmalloy@gmail.com>
2024-01-10 16:48:22 -0400

    Use messages instead of polling

--- src/main/java/jasper/component/scheduler/Async.java
+++ src/main/java/jasper/component/scheduler/Async.java
@@ -7,10 +7,13 @@ import jasper.domain.Ref_;
 import jasper.errors.NotFoundException;
 import jasper.repository.RefRepository;
 import jasper.repository.filter.RefFilter;
+import jasper.service.dto.RefDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -41,6 +44,7 @@ public class Async {
 	@Autowired
 	Ingest ingest;
 
+	Map<String, Boolean> changes = new HashMap<>();
 	Map<String, Instant> lastModified = new HashMap<>();
 
 	Map<String, AsyncWatcher> tags = new HashMap<>();
@@ -70,23 +74,42 @@ public class Async {
 		return String.join("|", plugins);
 	}
 
+	@ServiceActivator(inputChannel = "refRxChannel")
+	public void handleRefUpdate(Message<RefDto> message) {
+		var ref = message.getPayload();
+		if (ref.getTags() == null) return;
+		if (changes.get(ref.getOrigin()) != null) return;
+		if (!Arrays.asList(props.getAsyncOrigins()).contains(ref.getOrigin())) return;
+		for (var tag : ref.getTags()) {
+			if (tags.containsKey(tag) || responses.containsKey(tag)) {
+				changes.put(ref.getOrigin(), true);
+				return;
+			}
+		}
+	}
+
 	@Scheduled(
 		fixedRateString = "${jasper.async-interval-sec}",
 		initialDelayString = "${jasper.async-delay-sec}",
 		timeUnit = TimeUnit.SECONDS)
 	public void drainAsyncTask() {
+		if (changes.isEmpty()) return;
 		if (tags.isEmpty() && responses.isEmpty()) return;
 		for (var origin : props.getAsyncOrigins()) drain(origin);
 	}
 
 	private void drain(String origin) {
+		if (changes.get(origin) == null) return;
 		for (var i = 0; i < props.getAsyncBatchSize(); i++) {
 			var maybeRef = refRepository.findAll(RefFilter.builder()
 				.origin(origin)
 				.query(trackingQuery())
 				.modifiedAfter(lastModified.getOrDefault(origin, Instant.now().minus(1, ChronoUnit.DAYS)))
 				.build().spec(), PageRequest.of(0, 1, by(Ref_.MODIFIED)));
-			if (maybeRef.isEmpty()) return;
+			if (maybeRef.isEmpty()) {
+				changes.remove(origin);
+				return;
+			}
 			var ref = maybeRef.getContent().get(0);
 			lastModified.put(origin, ref.getModified());
 			tags.forEach((k, v) -> {
