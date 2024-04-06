fa429926616663ee8915b238e9262a73429a758c
Chris Malloy <cjmalloy@gmail.com>
2024-04-05 23:41:55 -0300

    Bugfix for user tag header with default auth

--- src/main/java/jasper/security/jwt/TokenProviderImplDefault.java
+++ src/main/java/jasper/security/jwt/TokenProviderImplDefault.java
@@ -7,6 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
+import static jasper.security.Auth.USER_TAG_HEADER;
+import static jasper.security.Auth.getHeader;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 public class TokenProviderImplDefault extends AbstractTokenProvider {
 	private final Logger logger = LoggerFactory.getLogger(TokenProviderImplDefault.class);
 
@@ -22,6 +26,10 @@ public class TokenProviderImplDefault extends AbstractTokenProvider {
 	@Override
 	public Authentication getAuthentication(String jwt, String origin) {
 		var principal = configs.security(origin).getDefaultUser() + origin;
+		if (props.isAllowUserTagHeader() && !isBlank(getHeader(USER_TAG_HEADER))) {
+			principal = getHeader(USER_TAG_HEADER);
+			logger.debug("User tag set by header: {} ({})", principal, origin);
+		}
 		var user = getUser(principal);
 		logger.debug("Default Auth {} {}", principal, origin);
 		return new PreAuthenticatedAuthenticationToken(principal, user, getAuthorities(user, origin));
