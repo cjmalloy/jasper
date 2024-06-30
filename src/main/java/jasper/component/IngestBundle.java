126a73e098173a10eb6efcb78db059618ab7399e
Chris Malloy <cjmalloy@gmail.com>
2024-06-30 14:37:07 -0300

    Don't stop ingesting if part of a bundle fails

--- src/main/java/jasper/component/IngestBundle.java
+++ src/main/java/jasper/component/IngestBundle.java
@@ -2,11 +2,14 @@ package jasper.component;
 
 import jasper.component.dto.Bundle;
 import jasper.errors.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IngestBundle {
+	private static final Logger logger = LoggerFactory.getLogger(IngestBundle.class);
 
 	@Autowired
 	Ingest ingestRef;
@@ -27,41 +30,61 @@ public class IngestBundle {
 		if (bundle.getRef() != null) for (var ref : bundle.getRef()) {
 			ref.setOrigin(origin);
 			try {
-				ingestRef.update(ref, false);
-			} catch (NotFoundException e) {
-				ingestRef.create(ref, false);
+				try {
+					ingestRef.update(ref, false);
+				} catch (NotFoundException e) {
+					ingestRef.create(ref, false);
+				}
+			} catch (Exception e) {
+				logger.error("Error ingesting Ref {}", ref.getUrl(), e);
 			}
 		}
 		if (bundle.getExt() != null) for (var ext : bundle.getExt()) {
 			ext.setOrigin(origin);
 			try {
-				ingestExt.update(ext, false);
-			} catch (NotFoundException e) {
-				ingestExt.create(ext, false);
+				try {
+					ingestExt.update(ext, false);
+				} catch (NotFoundException e) {
+					ingestExt.create(ext, false);
+				}
+			} catch (Exception e) {
+				logger.error("Error ingesting Ext {}", ext.getTag(), e);
 			}
 		}
 		if (bundle.getUser() != null) for (var user : bundle.getUser()) {
 			user.setOrigin(origin);
 			try {
-				ingestUser.update(user);
-			} catch (NotFoundException e) {
-				ingestUser.create(user);
+				try {
+					ingestUser.update(user);
+				} catch (NotFoundException e) {
+					ingestUser.create(user);
+				}
+			} catch (Exception e) {
+				logger.error("Error ingesting User {}", user.getTag(), e);
 			}
 		}
 		if (bundle.getPlugin() != null) for (var plugin : bundle.getPlugin()) {
 			plugin.setOrigin(origin);
 			try {
-				ingestPlugin.update(plugin);
-			} catch (NotFoundException e) {
-				ingestPlugin.create(plugin);
+				try {
+					ingestPlugin.update(plugin);
+				} catch (NotFoundException e) {
+					ingestPlugin.create(plugin);
+				}
+			} catch (Exception e) {
+				logger.error("Error ingesting Plugin {}", plugin.getTag(), e);
 			}
 		}
 		if (bundle.getTemplate() != null) for (var template : bundle.getTemplate()) {
 			template.setOrigin(origin);
 			try {
-				ingestTemplate.update(template);
-			} catch (NotFoundException e) {
-				ingestTemplate.create(template);
+				try {
+					ingestTemplate.update(template);
+				} catch (NotFoundException e) {
+					ingestTemplate.create(template);
+				}
+			} catch (Exception e) {
+				logger.error("Error ingesting Template {}", template.getTag(), e);
 			}
 		}
 	}
