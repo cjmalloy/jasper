1782084d07d5b6557727f01fd500a18c90a0dc4a
Chris Malloy <cjmalloy@gmail.com>
2024-08-10 02:12:20 -0300

    Fix async modified deadlock

--- src/main/java/jasper/component/delta/AsyncWebScraper.java
+++ src/main/java/jasper/component/delta/AsyncWebScraper.java
@@ -5,6 +5,7 @@ import jasper.component.Ingest;
 import jasper.component.Scraper;
 import jasper.component.Tagger;
 import jasper.domain.Ref;
+import jasper.errors.NotFoundException;
 import jasper.repository.RefRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +15,9 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
+
+import static jasper.domain.proj.HasOrigin.origin;
+import static jasper.domain.proj.Tag.matchesTag;
 
 @Profile("proxy")
 @Component
@@ -48,28 +52,28 @@ public class AsyncWebScraper implements Async.AsyncRunner {
 
 	@Override
 	public void run(Ref ref) throws Exception {
-		if (ref.hasTag("_plugin/delta/cache")) {
-			logger.debug("{} Waiting for async cache {}", ref.getOrigin(), ref.getUrl());
-			return;
-		}
-		if (!ref.hasTag("_plugin/cache")) {
-			logger.debug("{} Deferring scrape until cached {}", ref.getOrigin(), ref.getUrl());
-			tagger.tag(ref.getUrl(), ref.getOrigin(), "_plugin/delta/cache");
-			return;
-		}
 		logger.info("{} Scraping {}", ref.getOrigin(), ref.getUrl());
+		var tags = ref.getTags();
+		tagger.tag(ref.getUrl(), ref.getOrigin(), "-_plugin/delta/scrape");
 		var web = scraper.web(ref.getUrl(), ref.getOrigin());
+		// Fetch Ref again in case scrape modified it
+		ref = fetch(ref.getUrl(), ref.getOrigin());
 		em.detach(ref);
-		var scrapeAll = ref.hasTag("_plugin/delta/scrape/ref");
-		if (scrapeAll || ref.hasTag("_plugin/delta/scrape/title")) ref.setTitle(web.getTitle());
-		if (scrapeAll || ref.hasTag("_plugin/delta/scrape/comment")) ref.setComment(web.getComment());
-		if (scrapeAll || ref.hasTag("_plugin/delta/scrape/sources")) ref.setSources(web.getSources());
-		if (scrapeAll || ref.hasTag("_plugin/delta/scrape/alts")) ref.setAlternateUrls(web.getAlternateUrls());
-		if (scrapeAll || ref.hasTag("_plugin/delta/scrape/plugins")) ref.setPlugins(web.getPlugins());
-		if (scrapeAll || ref.hasTag("_plugin/delta/scrape/tags")) ref.setTags(web.getTags());
-		if (scrapeAll || ref.hasTag("_plugin/delta/scrape/published")) ref.setPublished(web.getPublished());
+		var scrapeAll = tags.stream().anyMatch(t -> matchesTag("_plugin/delta/scrape/ref", t));
+		if (scrapeAll || tags.stream().anyMatch(t -> matchesTag("_plugin/delta/scrape/title", t))) ref.setTitle(web.getTitle());
+		if (scrapeAll || tags.stream().anyMatch(t -> matchesTag("_plugin/delta/scrape/comment", t))) ref.setComment(web.getComment());
+		if (scrapeAll || tags.stream().anyMatch(t -> matchesTag("_plugin/delta/scrape/sources", t))) ref.setSources(web.getSources());
+		if (scrapeAll || tags.stream().anyMatch(t -> matchesTag("_plugin/delta/scrape/alts", t))) ref.setAlternateUrls(web.getAlternateUrls());
+		if (scrapeAll || tags.stream().anyMatch(t -> matchesTag("_plugin/delta/scrape/plugins", t))) ref.setPlugins(web.getPlugins());
+		if (scrapeAll || tags.stream().anyMatch(t -> matchesTag("_plugin/delta/scrape/tags", t))) ref.setTags(web.getTags());
+		if (scrapeAll || tags.stream().anyMatch(t -> matchesTag("_plugin/delta/scrape/published", t))) ref.setPublished(web.getPublished());
 		ref.removePrefixTags();
 		ref.removeTag("_plugin/delta/scrape");
 		ingest.update(ref, false);
+	}
+
+	private Ref fetch(String url, String origin) {
+		return refRepository.findOneByUrlAndOrigin(url, origin(origin))
+			.orElseThrow(() -> new NotFoundException("Async"));
 	}
 }
