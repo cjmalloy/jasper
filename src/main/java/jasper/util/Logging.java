f9211f65053b00fed81560ee80e2d16a1762272c
Chris Malloy <cjmalloy@gmail.com>
2025-09-15 02:34:27 -0300

    Fix simple logging loop

--- src/main/java/jasper/util/Logging.java
+++ src/main/java/jasper/util/Logging.java
@@ -2,7 +2,7 @@ package jasper.util;
 
 public class Logging {
 	public static String getMessage(Throwable e) {
-		if (e.getCause() != null) {
+		if (e.getCause() != null && e.getCause() != e) {
 			return e.getClass().getName() + " " + e.getMessage() + ": " + getMessage(e.getCause());
 		}
 		return e.getClass().getName() + " " + e.getMessage();
