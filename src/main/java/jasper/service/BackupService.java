ac784cb26e7048a6f41230e096cf0da954f9b18b
Chris Malloy <cjmalloy@gmail.com>
2025-09-16 18:48:11 -0300

    Auth not in scope

--- src/main/java/jasper/service/BackupService.java
+++ src/main/java/jasper/service/BackupService.java
@@ -112,7 +112,7 @@ public class BackupService {
 		var list = refRepository.findAll(isUrl("system:backup-key"));
 		for (var ref : list) {
 			if (ref.getCreated().isBefore(Instant.now().minus(15, ChronoUnit.MINUTES))) {
-				ingest.delete(auth.getOrigin(), "system:backup-key", ref.getOrigin());
+				ingest.delete(ref.getOrigin(), "system:backup-key", ref.getOrigin());
 			}
 		}
 	}
