1b59ab522b6159143b5046035a720014ca5521c7
Chris Malloy <cjmalloy@gmail.com>
2025-10-05 20:06:17 -0300

    Cleaned up

--- src/main/java/jasper/config/AsyncConfiguration.java
+++ src/main/java/jasper/config/AsyncConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.boot.autoconfigure.task.TaskSchedulingProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
--- src/main/java/jasper/config/RedisConfig.java
+++ src/main/java/jasper/config/RedisConfig.java
@@ -10,7 +10,6 @@ import jasper.service.dto.UserDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
