be39b52a05b4ade1aa50d8527093d76b74a822de
Chris Malloy <cjmalloy@gmail.com>
2025-10-25 17:11:47 -0300

    Allow turning off backfill

--- src/main/java/jasper/component/cron/Backfill.java
+++ src/main/java/jasper/component/cron/Backfill.java
@@ -7,11 +7,13 @@ import jasper.repository.RefRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 
+@Profile("!no-backfill")
 @Component
 public class Backfill {
 	private static final Logger logger = LoggerFactory.getLogger(Backfill.class);
