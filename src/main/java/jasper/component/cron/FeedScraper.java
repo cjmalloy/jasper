751182ada58d9c78ed6db8d4fb8fb00211ccf4dc
Chris Malloy <cjmalloy@gmail.com>
2025-11-25 23:44:02 -0400

    Fixed feed migration

--- src/main/java/jasper/component/cron/FeedScraper.java
+++ src/main/java/jasper/component/cron/FeedScraper.java
@@ -1,6 +1,7 @@
 package jasper.component.cron;
 
 import jakarta.annotation.PostConstruct;
+import jasper.component.ConfigCache;
 import jasper.component.script.RssParser;
 import jasper.domain.Ref;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Component;
 @Profile("scripts")
 @Component
 public class FeedScraper implements Cron.CronRunner {
+
+	@Autowired
+	ConfigCache config;
 
 	@Autowired
 	Cron cron;
@@ -25,6 +29,8 @@ public class FeedScraper implements Cron.CronRunner {
 
 	@Async
 	public void run(Ref ref) {
+		if (!config.root().script("plugin/feed", ref.getOrigin()) &&
+			!config.root().script("plugin/script/feed", ref.getOrigin())) return;
 		rssParser.runScript(ref, "plugin/feed");
 	}
 
