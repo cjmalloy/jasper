b3c6416e29b626b5b29e70f36bcb20cdd5ca12f1
Chris Malloy <cjmalloy@gmail.com>
2026-07-28 04:28:37 -0300

    Remove exception handling for forbidden operations
    Removed handling for OperationForbiddenOnOriginException in script execution.

--- src/main/java/jasper/component/delta/DeltaScript.java
+++ src/main/java/jasper/component/delta/DeltaScript.java
@@ -7,7 +7,6 @@ import jasper.component.ScriptExecutorFactory;
 import jasper.component.ScriptRunner;
 import jasper.component.Tagger;
 import jasper.domain.Ref;
-import jasper.errors.OperationForbiddenOnOriginException;
 import jasper.errors.UntrustedScriptException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -69,9 +68,6 @@ public class DeltaScript implements Async.AsyncRunner {
 				} catch (UntrustedScriptException e) {
 					logger.error("{} Script hash not whitelisted: {}", ref.getOrigin(), e.getScriptHash());
 					tagger.attachError(ref.getOrigin(), ref, "Script hash not whitelisted", e.getScriptHash());
-				} catch (OperationForbiddenOnOriginException e) {
-					logger.error("{} Script {} not installed: {}", ref.getOrigin(), scriptTag, e.getMessage());
-					tagger.attachError(ref.getOrigin(), ref, "Script " + scriptTag + " not installed", e.getMessage());
 				}
 			}).join();
 		}
