ea6a2fea920472c1d1516fadeb56fa837364958c
Chris Malloy <cjmalloy@gmail.com>
2026-07-28 20:57:42 -0300

    Remove error on missing scripts (#783)

--- src/main/java/jasper/component/cron/Script.java
+++ src/main/java/jasper/component/cron/Script.java
@@ -7,7 +7,6 @@ import jasper.component.ScriptExecutorFactory;
 import jasper.component.ScriptRunner;
 import jasper.component.Tagger;
 import jasper.domain.Ref;
-import jasper.errors.OperationForbiddenOnOriginException;
 import jasper.errors.UntrustedScriptException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -64,9 +63,6 @@ public class Script implements Cron.CronRunner {
 				} catch (UntrustedScriptException e) {
 					logger.error("{} Script hash not whitelisted: {}", ref.getOrigin(), e.getScriptHash());
 					tagger.attachError(ref.getOrigin(), ref, "Script hash not whitelisted", e.getScriptHash());
-				} catch (OperationForbiddenOnOriginException e) {
-					logger.error("{} Script {} not installed: {}", ref.getOrigin(), scriptTag, e.getMessage());
-					tagger.attachError(ref.getOrigin(), ref, "Script " + scriptTag + " not installed", e.getMessage());
 				}
 			}).join();
 		}
